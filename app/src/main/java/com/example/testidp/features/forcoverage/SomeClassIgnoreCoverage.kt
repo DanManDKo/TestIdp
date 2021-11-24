5f832de390f976e72c8ea19c2efd2af534326188
Danil <danyl.konovalenko@yalantis.net>
2021-11-24 18:47:42 +0200

    added func for testing

---
+++ app/src/main/java/com/example/testidp/features/forcoverage/SomeClassIgnoreCoverage.kt
@@ -0,0 +1,20 @@
+package com.example.testidp.features.forcoverage
+
+class SomeClassIgnoreCoverage {
+
+    fun addition(first: Int, second: Int): Int {
+        return first + second
+    }
+
+    fun subtraction(first: Int, second: Int): Int {
+        return first - second
+    }
+
+    fun multiplication(first: Int, second: Int): Int {
+        return first * second
+    }
+
+    fun division(first: Int, second: Int): Int {
+        return first / second
+    }
+}
